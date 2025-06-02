f8bfe340f3ae47f734c123ce260810f57cbf3d7b
Adan Luna <adan@oundmedia.com>
2025-06-02 09:37:36 -0600

    fix mail

--- resources/views/emails/payment/received.blade.php
+++ resources/views/emails/payment/received.blade.php
@@ -4,7 +4,23 @@
     Hemos recibido un depósito en tu cuenta con los siguientes detalles:
 
     - Monto: ${{ number_format($amount, 2) }} MXN
-    - Método de Pago: {{ ucfirst($paymentMethod) }}
+    - Método de Pago:
+    @switch($paymentMethod)
+        @case('card')
+            Tarjeta de crédito/débito
+        @break
+
+        @case('oxxo')
+            Pago en OXXO
+        @break
+
+        @case('customer_balance')
+            Transferencia Bancaria
+        @break
+
+        @default
+            {{ ucfirst($paymentMethod) }}
+    @endswitch
     - Fecha: {{ \Carbon\Carbon::parse($date)->format('d/m/Y H:i') }}
 
     Gracias por confiar en nosotros.
